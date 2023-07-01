8f0632eecdc9a5ef96b5c24ba9d6e456de24a587
manishaggarwal1908 <manishaggarwal1908>
2023-07-01 11:34:26 +0530

    Social_Media

---
+++ utils/username.js
@@ -0,0 +1,32 @@
+
+
+const ADJECTIVES = [
+    'boundless',
+    'plausible',
+    'sleepy',
+    'electronic',
+    'dangerous',
+    'purple',
+]
+
+const OBJECTS = [
+    'puddle',
+    'piano',
+    'window',
+    'bowl',
+    'socks',
+    'brocolli',
+    'chalk'
+]
+
+function genRandomUsername() {
+    const adj = ADJECTIVES[Math.floor(Math.random() * 7)]
+    const obj = OBJECTS[Math.floor(Math.random() * 7)]
+    return `${adj}-${obj}`
+}
+
+
+
+module.exports = {
+    genRandomUsername
+}
